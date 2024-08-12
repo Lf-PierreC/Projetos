9432abd09159b2b26ee12286c5e155fd66335422
Lf-PierreC <128261200+Lf-PierreC@users.noreply.github.com>
2024-08-12 01:56:05 -0300

    Add files via upload

---
+++ Em andamento/Portifólio/script.js
@@ -0,0 +1,21 @@
+// nivel estrelas
+
+function nivelEstrela() {
+    const estrela = document.querySelectorAll('.estrela')
+    let nivel = 0
+
+    for (let contEstrela = 0;){
+        c
+    }
+}
+
+const skills = document.querySelectorAll('.skill');
+
+for (let contSkill = 0; contSkill < skills.length; contSkill++){
+    const allskills = skills[contSkill];
+
+    allskills.onload = function(){
+        nivelEstrela();
+    }
+
+}
