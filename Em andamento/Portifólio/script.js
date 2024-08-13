6120b764b91930720766a2a590591536d105c863
Lf-PierreC <128261200+Lf-PierreC@users.noreply.github.com>
2024-08-13 00:58:01 -0300

    Add files via upload

--- Em andamento/Portifólio/script.js
+++ Em andamento/Portifólio/script.js
@@ -1,13 +1,16 @@
 // nivel estrelas
-
+/*
 function nivelEstrela() {
     const estrela = document.querySelectorAll('.estrela')
     let nivel = 0
 
-    for (let contEstrela = 0;){
-        c
+    for (let contEstrela = 0.2; contEstrela < estrela; contEstrela++){
+        if (nivel >= 0.5 && nivel < 1){
+            document.getElementById('vazia-01').style.display='none'
+            document.getElementById('meia-01').style.display='inline'
+        }
     }
-}
+}*/
 
 const skills = document.querySelectorAll('.skill');
 
@@ -15,7 +18,16 @@ for (let contSkill = 0; contSkill < skills.length; contSkill++){
     const allskills = skills[contSkill];
 
     allskills.onload = function(){
-        nivelEstrela();
+        const estrela = document.querySelectorAll('.estrela')
+    let nivel = 0
+
+    for (let contEstrela = 0.2; contEstrela < estrela; contEstrela++){
+        if (nivel >= 0.5 && nivel < 1){
+            document.getElementById('vazia-01').style.display='none'
+            document.getElementById('meia-01').style.display='inline'
+        }
+    }
+        //nivelEstrela();
     }
 
 }
